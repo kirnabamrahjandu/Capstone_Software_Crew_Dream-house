f557701f4a7e46f18347f7e93bde4ab3fdd95cce
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-19 00:48:44 +0530

    Chat_List_Class_IBAction_IBOutlet_Added

--- Dream House/View Controllers/ChatListVc.swift
+++ Dream House/View Controllers/ChatListVc.swift
@@ -2,27 +2,30 @@
 //  ChatListVc.swift
 //  Dream House
 //
-//  Created by Prince Kumar on 09/04/21.
+//  Created by Prince Kumar on 19/04/21.
 //
 
 import UIKit
 
-
-
 class ChatListVc: UIViewController {
 
-
+    @IBOutlet var chatListTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
+    }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-       
-    }
-    
+    */
 
 }
-
