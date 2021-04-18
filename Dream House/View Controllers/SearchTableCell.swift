a98075a94a123c1f8b50c1608f0bc4e112ffad6c
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-19 01:16:21 +0530

    Search_TableCell_Added

---
+++ Dream House/View Controllers/SearchTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  SearchTableCell.swift
+//  Dream House
+//
+//  Created by DreamHouseTeam Kumar on 09/04/21.
+//
+
+import UIKit
+
+class SearchTableCell: UITableViewCell {
+
+    @IBOutlet var historyLabel: UILabel!
+    
+    @IBOutlet var historyImage: UIImageView!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
