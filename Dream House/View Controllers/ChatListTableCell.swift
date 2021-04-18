3a6d0d6d48ccdc3aa4329505dc72a3abb34dae6c
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-19 00:38:14 +0530

    ChatList_TableCell_Class_added

---
+++ Dream House/View Controllers/ChatListTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChatListTableCell.swift
+//  Dream House
+//
+//  Created by Prince Kumar on 09/04/21.
+//
+
+import UIKit
+
+class ChatListTableCell: UITableViewCell {
+    
+    @IBOutlet var userNamelabel: UILabel!
+    
+    @IBOutlet var cellBackView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+       
+    }
+
+}
