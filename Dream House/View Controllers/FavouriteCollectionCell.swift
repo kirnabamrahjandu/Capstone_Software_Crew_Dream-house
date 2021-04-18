c15486cb0e7e9d153d5e694570ecadeddfc52ab0
sarabjeetkaur25 <infosarabjeetkaursidhu@gmail.com>
2021-04-19 00:26:28 +0530

    WishList_CollectionCell_Added

---
+++ Dream House/View Controllers/FavouriteCollectionCell.swift
@@ -0,0 +1,25 @@
+//
+//  FavouriteCollectionCell.swift
+//  Dream House
+//
+//  Created by Prince Kumar on 09/04/21.
+//
+
+import UIKit
+
+class FavouriteCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet var cellBackView: UIView!
+    
+    @IBOutlet var houseImage: UIImageView!
+    
+    @IBOutlet var ownerNameLabel: UILabel!
+    
+    @IBOutlet var favouriteBtn: UIButton!
+    
+    @IBOutlet var locationAddressLabel: UILabel!
+    
+    @IBOutlet var moneyPerMonthLabel: UILabel!
+    
+    
+}
