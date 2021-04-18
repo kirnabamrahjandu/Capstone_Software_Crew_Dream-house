cb989245268617d245252eb2d117721992fb9abd
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-19 01:20:09 +0530

    Search_Class_TableView_Delegate_Datasource_Added

--- Dream House/View Controllers/SearchVc.swift
+++ Dream House/View Controllers/SearchVc.swift
@@ -7,14 +7,16 @@
 
 import UIKit
 
-class SearchVc: UIViewController{
+class SearchVc: UIViewController, UITableViewDataSource, UITableViewDelegate{
+
 
     @IBOutlet var searchTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -26,5 +28,16 @@ class SearchVc: UIViewController{
     @IBAction func dismissBtnAction(_ sender: Any) {
       
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableCell") as! SearchTableCell
+        cell.searchHistoryLabel.text = "Searched \(indexPath.row)"
+        return cell
+    }
+    
 }
 
