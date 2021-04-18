1a6ce661ecac0b524082c5e67c2b2e7c1fda055d
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-19 00:36:51 +0530

    ChatList_Class_Added

---
+++ Dream House/View Controllers/ChatListVc.swift
@@ -0,0 +1,28 @@
+//
+//  ChatListVc.swift
+//  Dream House
+//
+//  Created by Prince Kumar on 09/04/21.
+//
+
+import UIKit
+
+
+
+class ChatListVc: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+    
+
+}
+
