1875b561e245d36e853c596ef3f9801e2dc57f7a
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-19 01:13:22 +0530

    Search_Class_Added

---
+++ Dream House/View Controllers/SearchVc.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVc.swift
+//  Dream House
+//
+//  Created by DreamHouseTeam Kumar on 07/04/21.
+//
+
+import UIKit
+
+class SearchVc: UIViewController{
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
+    
+
+
+    @IBAction func dismissBtnAction(_ sender: Any) {
+      
+    }
+}
+
