6f86a11a0edd75d8e37896125bda73650c6277aa
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-19 01:31:33 +0530

    AddHouse/Room_Class_IBAction_IBOutlet_Added

--- Dream House/View Controllers/AddHouseRoomVc.swift
+++ Dream House/View Controllers/AddHouseRoomVc.swift
@@ -10,13 +10,51 @@ import UIKit
 
 class AddHouseRoomVc: UIViewController {
 
+    @IBOutlet weak var postBtn: UIButton!
+    @IBOutlet var segmentBtn: UISegmentedControl!
+    @IBOutlet var houseNoTF: UITextField!
+    @IBOutlet var addressTF: UITextField!
+    @IBOutlet var imagesCollectionView: UICollectionView!
+    @IBOutlet var selectImageBtn: UIButton!
+    @IBOutlet var rentLabel: UITextField!
+    @IBOutlet var contactNumber: UITextField!
+    @IBOutlet var locationToBusStandTF: UITextField!
+    @IBOutlet var skipBtn: UIButton!
+    @IBOutlet weak var locationBtn: UIButton!
+    @IBOutlet weak var numberOfRooms: UITextField!
+    @IBOutlet weak var numberOfFloors: UITextField!
+    @IBOutlet weak var selectVideoBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
 
+    }
+    
+    @IBAction func selectImageAction(_ sender: Any) {
+       
+    }
+    
+    @IBAction func selectVideoAction(_ sender: Any) {
+      
+    }
+    
+    @IBAction func postAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func skipAction(_ sender: Any) {
+      
+    }
+    
+    @IBAction func locationBtnAction(_ sender: Any) {
+      
+}
+    
     
 }
     
