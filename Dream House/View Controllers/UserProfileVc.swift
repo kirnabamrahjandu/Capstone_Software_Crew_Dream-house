673df6f02f09d962e9989b4a27beedd27a95b7c0
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-20 02:13:21 +0530

    UserProfile_Class_Added

---
+++ Dream House/View Controllers/UserProfileVc.swift
@@ -0,0 +1,25 @@
+//
+//  UserProfileVc.swift
+//  Dream House
+//
+//  Created by DreamHouseTeam Kumar on 07/04/21.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileVc: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+ 
+
+}
