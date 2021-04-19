fa8bdacab6008e50aa7b2ca050cb61c58a4245a3
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-20 01:51:40 +0530

    Listing_detail_CollectionCell_Class_IBOutlet_Added

--- Dream House/View Controllers/AdvHomeDetailCollectionCell.swift
+++ Dream House/View Controllers/AdvHomeDetailCollectionCell.swift
@@ -7,7 +7,12 @@
 import UIKit
 
 class AdvHomeDetailCollectionCell: UICollectionViewCell {
-   
+    
+    @IBOutlet var cellBackView: UIView!
+    
+    @IBOutlet var advImageView: UIImageView!
+    
+    
     
     
 }
