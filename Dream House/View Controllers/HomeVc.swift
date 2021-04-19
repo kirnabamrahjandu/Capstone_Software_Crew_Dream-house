519c0267771df1f43ce9c520ae95738a0362b7a7
sarabjeetkaur25 <infosarabjeetkaursidhu@gmail.com>
2021-04-20 02:05:52 +0530

    HomePage_Data_Fetched_from_Firebase

--- Dream House/View Controllers/HomeVc.swift
+++ Dream House/View Controllers/HomeVc.swift
@@ -17,39 +17,67 @@ class HomeVc: UIViewController {
     
     @IBOutlet weak var searchTFBackView: UIView!
     
-    var model = [String]()
+    var model = [Rent]()
     var imgArray = [[String]]()
-
+    var protocolDelegate : hideTable?
+    var hideViewDelegate : dismissView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         homeCollectionView.delegate = self
         homeCollectionView.dataSource = self
-        searchTFBackView.layer.cornerRadius = 10
-      
-       
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        getFBdata()
 
     }
     
-
+ 
     @IBAction func searchLocationAction(_ sender: Any) {
     }
     
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
-
     }
     
     @IBAction func sortBtnAction(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "SortByVc") as! SortByVc
+       
         vc.modalPresentationStyle = .overCurrentContext
         self.present(vc, animated: true, completion: nil)
      }
     
-     
-
+    func getFBdata(){
+    
+          let ref = Database.database().reference().child("Rent")
+        ref.queryOrdered(byChild: "addType").queryEqual(toValue: "House").observe(.childAdded) { (DataSnapshot) in
+            if DataSnapshot.exists() {
+                
+                print(DataSnapshot, "is data")
+                let data = DataSnapshot.value as! [String:Any]
+                let dummyModel = Rent()
+                dummyModel.ownerName = data["ownerName"] as? String ?? ""
+                dummyModel.rent = data["Rent"]  as? String ?? ""
+                dummyModel.locationHouse = data["Location"]  as? String ?? ""
+                dummyModel.rooms = data["totalRooms"] as? String ?? ""
+                dummyModel.floors = data["totalFloors"] as? String ?? ""
+                dummyModel.locality = data["busStand"] as? String ?? ""
+                dummyModel.emailAddress = data["Email"] as? String ?? ""
+                dummyModel.contact = data["Phone"] as? String ?? ""
+                let imgArray = data["HouseImages"] as! [String]
+                self.imgArray.append(imgArray)
+                self.model.append(dummyModel)
+                self.homeCollectionView.reloadData()
+                               
+            }
+            else{
+              
+            }
+          }
+    
+      }
+  
 
 }
 
@@ -61,7 +89,24 @@ extension HomeVc : UICollectionViewDelegate, UICollectionViewDataSource, UIColle
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionCell", for: indexPath) as! HomeCollectionCell
-       
+        cell.ownerNameLabel1.text = model[indexPath.row].ownerName ?? "not found"
+        cell.moneyPerMonthLabel1.text = "$" + (model[indexPath.row].rent ?? "") + " / Month"
+        cell.cellBackView1.layer.cornerRadius = 10
+        let img = imgArray[indexPath.row].first
+        if let postedImage = img {
+                    let url = URL(string: postedImage)
+                    URLSession.shared.dataTask(with: url!, completionHandler: {(data, response, error) in
+                        if error != nil {
+                            print(error!)
+                        return
+                        }
+                        DispatchQueue.main.async {
+                            cell.houseImage1?.image = UIImage(data: data!)
+                        }
+                        }).resume()
+     }
+        cell.locationAddressLabel1.text = model[indexPath.row].locationHouse
+
         return cell
     }
     
@@ -70,7 +115,6 @@ extension HomeVc : UICollectionViewDelegate, UICollectionViewDataSource, UIColle
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (self.homeCollectionView.frame.width / 2) - (5), height: self.homeCollectionView.frame.height / 1.5)
     }
-    
     
 
 }
