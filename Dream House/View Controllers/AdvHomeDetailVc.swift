8a858c1ca70054d29c2b00bc07ec5d6171b9747a
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-20 01:46:54 +0530

    Listing(PostedHouse)_Details_Class_Added

---
+++ Dream House/View Controllers/AdvHomeDetailVc.swift
@@ -0,0 +1,22 @@
+//
+//  AdvHomeDetailVc.swift
+//  Dream House
+//
+//
+
+import UIKit
+import MessageUI
+
+class AdvHomeDetailVc: UIViewController, MFMailComposeViewControllerDelegate {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+  
+}
+
+
