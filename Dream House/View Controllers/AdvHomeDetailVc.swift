ca7ea9cc7fd6c442fbb0d1b46ba24e2c80680070
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-20 01:48:34 +0530

    Listing_Details_Class_IBAction_IBOutlet_Added

--- Dream House/View Controllers/AdvHomeDetailVc.swift
+++ Dream House/View Controllers/AdvHomeDetailVc.swift
@@ -9,14 +9,37 @@ import MessageUI
 
 class AdvHomeDetailVc: UIViewController, MFMailComposeViewControllerDelegate {
 
-
+    @IBOutlet var advHomeDetailCollectionView: UICollectionView!
     
+    @IBOutlet var totalBedroomsTF: UITextField!
+    
+    @IBOutlet var totalFloorsTF: UITextField!
+    
+    @IBOutlet var rentPerMonth: UITextField!
+    
+    @IBOutlet var busStandLoc: UITextField!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+
     }
     
-  
+    
+    @IBAction func callAction(_ sender: Any) {
+    
+    }
+    
+    @IBAction func emailAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func messageAction(_ sender: Any) {
+
+        
+    }
+    
+ 
 }
 
 
