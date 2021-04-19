573df69a9c447e9785d5cd91d72cf000a0cf47cf
kirnabamrahjandu <kirna1851994@gmail.com>
2021-04-20 00:32:56 +0530

    Home_Screen_IBAction_IBOUTLets_Added

--- Dream House/View Controllers/HomeVc.swift
+++ Dream House/View Controllers/HomeVc.swift
@@ -8,29 +8,44 @@ import UIKit
 
 
 class HomeVc: UIViewController {
+    
+    @IBOutlet var houseRoomSegmentBtn: UISegmentedControl!
+    
+    @IBOutlet var homeCollectionView: UICollectionView!
+    
+    @IBOutlet var searchTF: UITextField!
+    
+    @IBOutlet weak var searchTFBackView: UIView!
 
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
        
     }
     
     override func viewWillAppear(_ animated: Bool) {
+    
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
       
     }
     
-
     @IBAction func searchLocationAction(_ sender: Any) {
+        
     }
     
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
- 
+
     }
     
     @IBAction func sortBtnAction(_ sender: Any) {
 
      }
-    
-}
 
+    
+
+
+}
 
