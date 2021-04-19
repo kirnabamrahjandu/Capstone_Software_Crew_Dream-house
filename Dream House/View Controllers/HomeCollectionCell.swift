005b9fd25efd364e79f972de684bdb7c0414601f
kirnabamrahjandu <kirna1851994@gmail.com>
2021-04-20 00:34:58 +0530

    Home_Screen_Collection_Cell_Added

---
+++ Dream House/View Controllers/HomeCollectionCell.swift
@@ -0,0 +1,25 @@
+//
+//  HomeCollectionCell.swift
+//  Dream House
+//
+//
+
+import UIKit
+
+class HomeCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet var cellBackView: UIView!
+    
+    @IBOutlet var ownerNameLabel: UILabel!
+    
+    @IBOutlet var houseImage: UIImageView!
+   
+    @IBOutlet var locationAddressLabel: UILabel!
+    
+    @IBOutlet var moneyPerMonthLabel: UILabel!
+    
+    @IBOutlet var favouriteBtn: UIButton!
+    
+    
+    
+}
