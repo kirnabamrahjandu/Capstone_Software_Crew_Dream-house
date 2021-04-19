c07bf8ccc272d4a62e4584d06b2d151a90b6fb70
kirnabamrahjandu <kirna1851994@gmail.com>
2021-04-20 00:30:56 +0530

    home_Screen_Class_added

---
+++ Dream House/View Controllers/HomeVc.swift
@@ -0,0 +1,36 @@
+//
+//  HomeVc.swift
+//  Dream House
+//
+//
+
+import UIKit
+
+
+class HomeVc: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      
+    }
+    
+
+    @IBAction func searchLocationAction(_ sender: Any) {
+    }
+    
+    @IBAction func segmentAction(_ sender: UISegmentedControl) {
+ 
+    }
+    
+    @IBAction func sortBtnAction(_ sender: Any) {
+
+     }
+    
+}
+
+
