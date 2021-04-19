cd472792bcaa2975f2eba600dc490267caeab542
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-20 02:14:17 +0530

    UserProfile_Class_IBOutlet_Added

--- Dream House/View Controllers/UserProfileVc.swift
+++ Dream House/View Controllers/UserProfileVc.swift
@@ -10,6 +10,13 @@ import Firebase
 
 class UserProfileVc: UIViewController {
 
+    @IBOutlet var addProfileImage: UIImageView!
+    
+    @IBOutlet var userNameTF: UITextField!
+    
+    @IBOutlet var userTypeTF: UITextField!
+    
+    @IBOutlet var eMailTF: UITextField!
     
     
     override func viewDidLoad() {
@@ -18,8 +25,9 @@ class UserProfileVc: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-       
+        
     }
- 
+
+
 
 }
