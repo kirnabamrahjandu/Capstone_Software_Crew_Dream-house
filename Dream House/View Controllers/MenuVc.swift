aaabf4a610a51a5971bdbb584789a0c044ee32d7
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-04-20 02:19:04 +0530

    SideBar_UserSide_Class_Added

---
+++ Dream House/View Controllers/MenuVc.swift
@@ -0,0 +1,26 @@
+//
+//  MenuVc.swift
+//  Dream House
+//
+//
+
+import UIKit
+
+class MenuVc: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+
+    }
+    
+    @IBAction func backBtnAction(_ sender: Any) {
+        
+    }
+    
+}
+
