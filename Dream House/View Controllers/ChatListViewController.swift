bb930345b3cd851c7c2de28bb2a0b4d8d7afe229
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-21 02:16:22 +0530

    Chat_List_Class_IBOutlet_Added

---
+++ Dream House/View Controllers/ChatListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatListViewController.swift
+//  Dream House
+//
+//
+
+import UIKit
+
+class ChatListVc: UIViewController {
+
+    @IBOutlet var chatListTableView: UITableView!
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+    }
+    
+
+}
+
+
