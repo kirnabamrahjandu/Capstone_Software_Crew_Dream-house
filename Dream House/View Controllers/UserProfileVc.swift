f1b25834a5542270572a7dc32eb8fe8609b351e2
kirnabamrahjandu <kirna1851994@gmail.com>
2021-04-21 03:37:28 +0530

    Sidebar_Added_in_UserProfile

--- Dream House/View Controllers/UserProfileVc.swift
+++ Dream House/View Controllers/UserProfileVc.swift
@@ -27,12 +27,36 @@ class UserProfileVc: UIViewController {
         self.eMailTF.text = Auth.auth().currentUser?.email
         self.userNameTF.text = username
         self.userTypeTF.text = userType
-     
+        menuIcon()
         self.navigationController?.navigationBar.topItem?.title = ""
         self.navigationItem.title = "My Profile"
     }
 
 
-
+    func menuIcon(){
+        
+        let dp = userProfileURL
+        let postedImage = dp
+        let url = URL(string: postedImage)
+        URLSession.shared.dataTask(with: url!, completionHandler: {(data, response, error) in
+            if error != nil {
+                print(error!)
+                return
+            }
+            DispatchQueue.main.async {
+                self.addProfileImage.image = UIImage(data: data!)
+            }
+        }).resume()
+        
+        
+        let navBarPlusButton = UIBarButtonItem(image: UIImage(named:  "menu"),  style: .plain, target: self, action: #selector(tabNavigations))
+        navigationItem.leftBarButtonItem = navBarPlusButton
+    }
+    
+    @objc func tabNavigations(){
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "MenuVc") as! MenuVc
+        vc.ref = self
+        self.transitionVc(vc: vc, duration: 0.1, type: .fromLeft)
+    }
 
 }
