b8c589a1d46c946f03d3eec0873085a6cfe819b3
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-21 02:31:24 +0530

    Messages_Class_TableViewDelegate_DataSource_Added

--- Dream House/View Controllers/MessageListVc.swift
+++ Dream House/View Controllers/MessageListVc.swift
@@ -14,7 +14,22 @@ class MessageListVc: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
     }
+    
+}
+
+extension MessageListVc : UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell") as! MessageCell
+        cell.label.text = "Dummy Message"
+        return cell
+    }
+    
     
 }
