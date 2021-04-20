2abaeae99ad661e74a2f46f080f89aedd05f2e39
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-21 02:27:54 +0530

    Messages_XIB_Class(TableCell)_Added

---
+++ Dream House/View Controllers/TableCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableCell.swift
+//  Dream House
+//
+//
+
+import Foundation
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
