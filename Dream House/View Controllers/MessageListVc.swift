5a85c61e8d9d1494ac3fe5e947699d6ebae9630e
Harpreet777-cloud <harpreet1998kaur31@gmail.com>
2021-04-21 02:24:47 +0530

    Messages_Class_IBOutlet_Added

--- Dream House/View Controllers/MessageListVc.swift
+++ Dream House/View Controllers/MessageListVc.swift
@@ -2,12 +2,15 @@
 //  MessageListVc.swift
 //  Dream House
 //
-//  Created by Prince Kumar on 21/04/21.
 //
 
 import UIKit
 
 class MessageListVc: UIViewController {
-   
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
     
 }
